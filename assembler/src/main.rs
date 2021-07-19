abdb9be51dbd50fb144806a6ae451382599b83cb
GabrielDertoni <gab.dertoni@usp.br>
2021-07-19 13:53:37 -0300

    Add cli functionalities

--- assembler/src/main.rs
+++ assembler/src/main.rs
@@ -6,10 +6,9 @@
 #![feature(maybe_uninit_extra)]
 #![feature(maybe_uninit_uninit_array)]
 
-use clap::clap_app;
-
-use std::io::Write;
 use std::fs;
+
+use clap::clap_app;
 
 mod ast;
 mod parser;
@@ -17,8 +16,8 @@ mod codegen;
 mod error;
 mod utils;
 
-// use crate::parser::parse_zasm;
-// use crate::codegen::Assembler;
+use crate::parser::parse_src;
+use crate::codegen::Assembler;
 
 fn main() -> std::io::Result<()> {
 
@@ -28,15 +27,27 @@ fn main() -> std::io::Result<()> {
         (about: "A compiler for the ZASM programming language")
         (@arg SOURCE: +required "The ZASM source file to compile")
         (@arg output: -o --output +takes_value "Output compiled tape")
-        (@arg expand: -E --expand "Compile and also print the desugared code")
+        // (@arg expand: -E --expand "Compile and also print the desugared code")
     ).get_matches();
 
     // Ok, SOURCE is required.
     let src_file = matches.value_of("SOURCE").unwrap();
     let out = matches.value_of("output").unwrap_or("a.out");
-    let expand = matches.is_present("expand");
+    // let expand = matches.is_present("expand");
 
     let source = fs::read_to_string(src_file)?;
+
+    match parse_src(&source) {
+        Err(e)   => eprintln!("{}", e),
+        Ok(prog) => {
+            let file = fs::File::create(out)?;
+            let mut assembler = Assembler::new(file, false);
+
+            if let Err(e) = assembler.assemble(&prog.stmts) {
+                eprintln!("{}", e);
+            }
+        }
+    }
 
     Ok(())
 }
