a541f110ca11946903df7336abb7cbdf755b1265
GabrielDertoni <gab.dertoni@usp.br>
2021-08-05 15:52:39 -0300

    Add GET_RAND software interrupt

--- simulator/src/os.rs
+++ simulator/src/os.rs
@@ -8,6 +8,7 @@ pub const PRINT_CHAR: i16    = 3;
 pub const PRINT_DECIMAL: i16 = 4;
 pub const PRINT_BINARY: i16  = 5;
 pub const PRINT_HEX: i16     = 6;
+pub const GET_RAND: i16      = 7;
 
 pub fn run_interrupt<IO>(reg_bank: &mut RegBank, mut io_device: IO) -> io::Result<()>
 where
@@ -20,6 +21,7 @@ where
         PRINT_DECIMAL => write!(io_device, "{}", reg_bank[Reg::R1]),
         PRINT_BINARY  => write!(io_device, "{:b}", reg_bank[Reg::R1]),
         PRINT_HEX     => write!(io_device, "{:x}", reg_bank[Reg::R1]),
+        GET_RAND      => Ok(reg_bank[Reg::ACC] = rand::random()),
         n             => {
             Err(io::Error::new(ErrorKind::Unsupported, format!("Unexpected system call: {}.", n)))
         },
